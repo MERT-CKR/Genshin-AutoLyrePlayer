0cf5c43fccf4683d03dcf9948f84975dcd66083a
Mert Çakır <82875825+MERT-CKR@users.noreply.github.com>
2024-09-27 20:34:01 +0300

    Tempo support added

--- columnPlayer.py
+++ columnPlayer.py
@@ -17,41 +17,50 @@ def countDown():
     print("Starting... ")
 
 
+tempo_dict={
+    0:1,
+    1:2,
+    2:4,
+    3:8
+}
 def play_music(sheets,bpm):
     global replaced_elements
 
-    print("bpm",bpm)
-    beats_per_sec = bpm/60
-    beats_per_sec -= beats_per_sec *-0.19
-    wait = 1/beats_per_sec
+    print("bpm",bpm)#bpm: beats per minute | bps: peats per second
+    bps = bpm/60
+    # bps -= bps *-0.10 # %10 play speed
+    wait = 1/bps
     
     countDown()
     t1 = time.time()
     
     for i in sheets:
+        tempo = tempo_dict[i[0]]
+        wait_among_notes = wait/tempo
+        
         if i[1] == []:
             replaced_elements = "Empty Page"
         else:
             first_elements = [item[0] for item in i[1]]
             replaced_elements = [keys[numbers.index(str(elem))] if str(elem) in numbers else str(elem) for elem in first_elements]
 
-        print(replaced_elements)
+        print(f"{replaced_elements} Tempo: {tempo}")
         print("-----------")
 
         if keyboard.is_pressed('"'):
             print("loop ending")
             break
         
-        if replaced_elements != "Empty Page":
+        if "Empty Page" not in replaced_elements  :
             if len(replaced_elements) > 1:
                 for char in replaced_elements:
                     keyboard.press_and_release(char)
-                time.sleep(wait)
+                time.sleep(wait_among_notes)
             else:
                 keyboard.press_and_release(replaced_elements[0])
-                time.sleep(wait)
+                time.sleep(wait_among_notes)
         else:
-            time.sleep(wait)
+            time.sleep(wait_among_notes)
     t2=time.time()
     playtime = str(t2-t1)[0:4]
     print("playback duration",playtime)
